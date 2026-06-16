21cdb994260abc52428eb0769c18e02a83cd90ca
lagfake28-afk <lagfake28@gmail.com>
2026-06-16 11:00:22 +0600

    Add files via upload

---
+++ send.php
@@ -0,0 +1,27 @@
+<?php
+
+$botToken = "8353093396:AAEu22kcF4PLhYC-K68woIsW9bGNpv33pZI";
+$chatId   = "7931320938";
+
+$message = $_POST['message'];
+
+$url = "https://api.telegram.org/bot$botToken/sendMessage";
+
+$data = [
+    "chat_id" => $chatId,
+    "text" => "From Website:\n" . $message
+];
+
+$options = [
+    "http" => [
+        "header"  => "Content-type: application/x-www-form-urlencoded",
+        "method"  => "POST",
+        "content" => http_build_query($data)
+    ]
+];
+
+$context = stream_context_create($options);
+file_get_contents($url, false, $context);
+
+echo "OK";
+?>
